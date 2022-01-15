2f3b4ab56429abb2d24e6421d6cbb4712ddf0106
Mark Enkema <markenkema@gmail.com>
2022-01-14 18:57:41 -0800

    updated utils

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,3 +1,5 @@
+use std::borrow::BorrowMut;
+
 // Definition for singly-linked list.
 #[derive(PartialEq, Eq, Clone, Debug)]
 pub struct ListNode {
@@ -7,10 +9,23 @@ pub struct ListNode {
 
 impl ListNode {
   #[inline]
-  fn new(val: i32) -> Self {
+  pub fn new(val: i32) -> Self {
     ListNode {
       next: None,
       val
     }
+  }
+}
+
+impl From<Vec<i32>> for ListNode {
+  fn from(v: Vec<i32>) -> Self {
+     let mut temp = ListNode::new(0);
+     let mut tar = &mut temp.next;
+     for num in v {
+      *tar = Some(Box::new(ListNode::new(num)));
+      tar = &mut tar.as_mut().unwrap().next;
+     }
+
+     *temp.next.unwrap()
   }
 }
